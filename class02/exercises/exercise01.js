13955ec41c13919bf833a5779525ff40d4013216
KyleneA <126809272+KyleneA@users.noreply.github.com>
2026-04-23 14:54:23 -0400

    Adding my exercise01 solution

---
+++ class02/exercises/exercise01.js
@@ -0,0 +1,24 @@
+// load DOM elements
+const input = document.getElementById("name-input");
+const button = document.getElementById("greet-button");
+const output = document.getElementById("output");
+
+// add event listener to button
+button.addEventListener("click", () => {
+    const name = input.value.trim();
+
+    if (name === "") {
+        output.textContent = "Please enter something";
+        return;
+    }
+
+    setTimeout(() => {
+        output.textContent = `Hello, ${name}`;
+    }, 1000);
+})
+
+    // continuation function
+        // take input value + trim
+        // validate not empty
+            // if empty, update output to "Please enter something"
+        // after 1000ms, update output to "Hello, input_value"
