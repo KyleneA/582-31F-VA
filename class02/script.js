1e98900b4a9ae38948e9b374cec5166a6304adb8
KyleneA <126809272+KyleneA@users.noreply.github.com>
2026-04-23 14:40:30 -0400

    Adding DOM and event handling review

--- class02/script.js
+++ class02/script.js
@@ -23,3 +23,47 @@ doSomethingLater(() => {
     console.log("Hello");
 });
 
+/* DOM Review */
+const title = document.getElementById("title");
+const description = document.querySelector("p.description");
+const output = document.getElementById("output");
+
+console.log(title, description.textContent, output);
+
+//updating DOM content
+output.textContent = "Add something new here";
+
+const topicsList = document.getElementById("topic-list");
+const topics = ["one", "two", "three"];
+
+for (const topic of topics){
+    const li = document.createElement("li");
+    li.textContent = topic;
+
+    topicsList.append(li);
+}
+
+// Event handling
+const button = document.getElementById("my-button");
+console.log(button);
+
+// button.addEventListener("click", () => {
+//     console.log("button is clicked");
+// });
+
+const nameInput = document.getElementById("name-input");
+
+button.addEventListener("click", () => {
+    const name = nameInput.value.trim();
+
+    output.textContent = "Hello there, " + name;
+});
+
+// input event
+const previewOutput = document.getElementById("preview-output");
+
+nameInput.addEventListener("input", () => {
+    console.log("typing in input..");
+
+    previewOutput.textContent = `Typing: ${nameInput.value}`;
+});
