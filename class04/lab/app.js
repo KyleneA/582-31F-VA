a52bdbe388580684d03b057fbc1578d232023969
KyleneA <126809272+KyleneA@users.noreply.github.com>
2026-04-30 15:15:07 -0400

    Adding part 3 of lab

--- class04/lab/app.js
+++ class04/lab/app.js
@@ -4,15 +4,15 @@ const courseDiv = document.getElementById("course-data");
 const courseBtn = document.getElementById("courses-btn");
 const clearBtn = document.getElementById("clear");
 
-const student1 = {
+const student = {
     id: "12345",
-    name: "John Dough",
+    name: "Jon Dough",
     program: "Web Design",
     semester: "winter 2026",
     bio: "Something about John and his accomplishments as a student.",
 };
 
-function getStudentData(student) {
+function getStudentData() {
     const isStudent = student.id && student.name && student.program && student.semester && student.bio;
     
     const promise = new Promise((resolve, reject) => {
@@ -25,4 +25,24 @@ function getStudentData(student) {
     return promise;
 }
 
-getStudentData(student1);
+getStudentData();
+
+function renderStudent(student) {
+    const title = document.createElement("h2");
+    const studentName = document.createElement("h3");
+    const program = document.createElement("p");
+    const semester = document.createElement("p");
+    const bio = document.createElement("p");
+
+    title.textContent = "Student Profile";
+    studentDiv.append(title);
+
+    studentName.textContent = student.name;
+    program.textContent = `Program: ${student.program}`;
+    semester.textContent = `Semester: ${student.semester}`;
+    bio.textContent = `Bio: ${student.bio}`;
+
+    studentDiv.append(studentName, program, semester, bio);
+}
+
+renderStudent(student);
