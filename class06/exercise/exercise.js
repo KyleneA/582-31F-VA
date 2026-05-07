4cfd9f245a2cc320a4478236d70931952f5b4dfa
KyleneA <126809272+KyleneA@users.noreply.github.com>
2026-05-07 14:36:11 -0400

    Adding my solution for exercises 1 & 2

---
+++ class06/exercise/exercise.js
@@ -0,0 +1,20 @@
+// Exercise 1
+const jsonText = '{"title":"Web Interface Programming 2","credits":3,"active":true}';
+
+const parsedJSON = JSON.parse(jsonText);
+
+console.log("object:", parsedJSON);
+console.log("title:", parsedJSON.title);
+console.log("credits:", parsedJSON.credits);
+
+// Exercise 2
+const course = {
+    title: "Advanced Programming",
+    credits: 3,
+    active: true
+};
+
+const courseJSON = JSON.stringify(course, null, 2);
+
+console.log("result:", courseJSON);
+console.log("type:", typeof courseJSON);
