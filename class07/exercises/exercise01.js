c56fe252cf4b6077bbacb7053d5a15474cad86cf
KyleneA <126809272+KyleneA@users.noreply.github.com>
2026-05-13 14:58:27 -0400

    Adding JS interactivity and additional styling

--- class07/exercises/exercise01.js
+++ class07/exercises/exercise01.js
@@ -1,3 +1,36 @@
 const loadPostBtn = document.getElementById("load-btn");
 const statusP = document.getElementById("status");
 const postDiv = document.getElementById("post-content");
+
+loadPostBtn.addEventListener("click", () => {
+    statusP.textContent = "Loading post...";
+    postDiv.innerHTML = "";
+
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+        .then((response) => {
+            if (!response.ok){
+                throw new Error(`HTTP ${response.status} Error`);
+            }
+
+            return response.json();
+        })
+        .then ((post) => {
+            console.log(post);
+            const titleH2 = document.createElement("h2");
+            titleH2.textContent = post.title;
+            titleH2.className = "post-title";
+            
+            const bodyP = document.createElement("p");
+            bodyP.textContent = post.body;
+
+            setTimeout(() => {
+                statusP.textContent = "Post was successfully loaded";
+                postDiv.append(titleH2, bodyP);
+            }, 1000);
+        })
+        .catch((error) => {
+            console.log(error);
+            statusP.textContent = "Post failed to load";
+            postDiv.textContent = error;
+        });
+});
