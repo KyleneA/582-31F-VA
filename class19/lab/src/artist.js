66c719880c4229c7e738037b037ed33413a3f441
KyleneA <126809272+KyleneA@users.noreply.github.com>
2026-07-09 15:59:13 -0400

    Creating Artist class

---
+++ class19/lab/src/artist.js
@@ -0,0 +1,39 @@
+export default class Artist {
+    constructor(id, name, genre, stage, time, country, headliner) {
+        this.__id = id;
+        this.__name = name;
+        this.genre = genre;
+        this.stage = stage;
+        this.time = time;
+        this.country = country;
+        this.headliner = headliner;
+    }
+
+    get summary() {
+        return `${this.__name} (${this.country}) | ${this.genre} | ${this.stage} - ${this.time}`;
+    }
+
+    set headliner(value) {
+        if (typeof(value) === "boolean") {
+            this.__headliner = value;
+        } else {
+            throw new Error("Headliner must be boolean value.")
+        }
+    }
+
+    static fromObject(data) {
+        const id = data.id || undefined;
+        const name = data.name || undefined;
+        const genre = data.genre || undefined;
+        const stage = data.stage || undefined;
+        const time = data.time || undefined;
+        const country = data.country || undefined;
+        const headliner = data.headliner;
+
+        if (!id || !name || !genre || !stage || !time || !country || headliner === "") {
+            throw new Error("Artist instance was not created. Invalid data.")
+        } else {
+            return new this(id, name, genre, stage, time, country, headliner)
+        }
+    }
+}
